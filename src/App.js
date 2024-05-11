d2e2a94abac39020c4105ac52eaf3d3436ddc772
Thaileshsinha <sinhathailesh@gmail.com>
2024-05-11 19:05:39 +0530

    add in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [val, setVal] = useState([]);
+  const [otherVal, setOtherVal] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+
+  const colorDetails = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "black",
+    "brown",
+  ];
+
+  useEffect(() => {
+    initialData();
+  }, []);
+
+  const initialData = async () => {
+    const resp = await axios.get(
+      "https://8fd85044d6f4480a8a44b0838cc19de9.api.mockbin.io"
+    );
+    const skip = (page - 1).limit;
+    setVal(resp.data);
+    setOtherVal(resp.data.slice(0, 10));
+  };
+
+  const changeLimit = async () => {
+    setLimit(limit + 10);
+    setPage(page + 10);
+    setOtherVal(val.slice(page, limit));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      <button onClick={changeLimit}>change limit</button>
+      {otherVal?.map((e, index) => (
+        <p
+          key={index}
+          style={{ color: colorDetails[Math.floor((index + page) / 7)] }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {e.name}
+        </p>
+      ))}
+    </main>
   );
 }
-
-export default App;
